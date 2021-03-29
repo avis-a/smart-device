3a8403e98fae97c84fb736bf4d146880ab37c622
Alina Glebova <alina.lina2407@gmail.com>
2021-03-30 01:54:14 +0300

    Добавлен local storage

--- source/js/main.js
+++ source/js/main.js
@@ -12,6 +12,11 @@
   var modalButtonClose = document.querySelector('.modal__button--close');
   var modalOverlay = document.querySelector('.modal__overlay');
 
+  var modalF = document.querySelector('.modal__form');
+  var modalName = document.getElementById('modal-name');
+  var modalTel = document.getElementById('modal-tel');
+  var modalQuestion = document.getElementById('modal-question');
+
   // аккордеон
   closeButtons.forEach(function (closeButton){
     if (closeButton) {
@@ -35,13 +40,24 @@
   mainNavButton.addEventListener('click', function (evt) {
     modalForm.classList.remove('visually-hidden');
     document.getElementById('modal-name').focus();
+
     document.addEventListener(`keydown`, escPress);
     modalButtonClose.addEventListener('click', closeModal);
     modalOverlay.addEventListener('click', closeModal);
+
+    let localItem = localStorage.getItem('modal-form-data');
+    if(localItem) {
+      let localData = JSON.parse(localItem);
+
+      modalName.value = localData.name;
+      modalTel.value = localData.tel;
+      modalQuestion.value = localData.question;
+    }
   });
 
   const closeModal = () => {
     modalForm.classList.add('visually-hidden');
+
     modalButtonClose.removeEventListener('click', closeModal);
     modalOverlay.removeEventListener('click', closeModal);
     document.removeEventListener(`keydown`, escPress);
@@ -54,8 +70,26 @@
     }
   };
 
+  const modalInputChange = () => {
+    let dataObj = {
+      name: modalName.value,
+      tel: modalTel.value,
+      question: modalQuestion.value
+    };
+
+    localStorage['modal-form-data'] = JSON.stringify(dataObj);
+  };
+
+  modalName.addEventListener('change', modalInputChange);
+  modalTel.addEventListener('change', modalInputChange);
+  modalQuestion.addEventListener('change', modalInputChange);
+
+  modalF.addEventListener('submit', () => {
+    localStorage.removeItem('modal-form-data');
+  });
+
   // маска поля телефон
-  var maskConf = {
+  const maskConf = {
     mask: '+{7}(000)000-00-00'
   };
   IMask(document.getElementById('tel'), maskConf);
