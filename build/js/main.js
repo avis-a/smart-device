165d331e41abc5a844b6608b39f826b61851253e
Alina Glebova <alina.lina2407@gmail.com>
2021-03-29 22:45:08 +0300

    Скорректировала кнопку в блоке info и блок с годами в футере

--- build/js/main.js
+++ build/js/main.js
@@ -2,60 +2,67 @@
 
 (function () {
 
-  var closeButton = document.querySelector('.close-button');
-  // var searchForm = document.querySelector('.form');
-  // var submitButton = document.querySelector('.form__button');
+  var closeButtons = document.querySelectorAll('.close-button');
+  var searchForm = document.querySelector('.form');
+  var submitButton = document.querySelector('.form__button');
 
-  if (closeButton) {
-    closeButton.classList.remove('close-button--opened');
-    closeButton.classList.add('close-button--closed');
-    closeButton.classList.remove('close-button--nojs');
+  closeButtons.forEach(function (closeButton){
+    if (closeButton) {
+      closeButton.classList.remove('close-button--opened');
+      closeButton.classList.add('close-button--closed');
+      closeButton.classList.remove('close-button--nojs');
 
-    closeButton.addEventListener('click', function () {
-      if (closeButton.classList.contains('close-button--closed')) {
-        closeButton.classList.remove('close-button--closed');
-        closeButton.classList.add('close-button--opened');
-      } else {
-        closeButton.classList.add('close-button--closed');
-        closeButton.classList.remove('close-button--opened');
+      closeButton.addEventListener('click', function () {
+        if (closeButton.classList.contains('close-button--closed')) {
+          closeButton.classList.remove('close-button--closed');
+          closeButton.classList.add('close-button--opened');
+        } else {
+          closeButton.classList.add('close-button--closed');
+          closeButton.classList.remove('close-button--opened');
+        }
+      });
+    }
+  });
+
+  var phoneMask = IMask(
+    document.getElementById('tel'), {
+      mask: '+{7}(000)000-00-00'
+    });
+
+
+  if (searchForm) {
+    submitButton.addEventListener('click', function (evt) {
+
+      var isValidateSuccess = true;
+
+      // получаем поля формы
+      var name = document.getElementById('name');
+      var tel = document.getElementById('tel');
+
+      name.setCustomValidity('');
+      tel.setCustomValidity('');
+
+      if (!name.value) {
+        isValidateSuccess = false;
+        name.setCustomValidity('Имя не может быть пустым.');
+      }
+
+      var regexResult = tel.value.match(/(\+?\d[- .]*){7,13}/i);
+
+      if (!tel.value) {
+        isValidateSuccess = false;
+        tel.setCustomValidity('Телефон не может быть пустым.');
+      } else if (regexResult === null || regexResult.length <= 0) {
+        isValidateSuccess = false;
+        tel.setCustomValidity('Значение поля "Телефон" не удовлетворяет шаблону!');
+      }
+
+      if (isValidateSuccess !== true) {
+        name.reportValidity();
+        tel.reportValidity();
+
+        evt.preventDefault();
       }
     });
   }
-
-
-  // if (searchForm) {
-  //   submitButton.addEventListener('click', function (evt) {
-
-  //     var isValidateSuccess = true;
-
-  //     // получаем поля формы
-  //     var name = document.getElementById('name');
-  //     var tel = document.getElementById('tel');
-
-  //     name.setCustomValidity('');
-  //     tel.setCustomValidity('');
-
-  //     if (!name.value) {
-  //       isValidateSuccess = false;
-  //       name.setCustomValidity('Имя не может быть пустым.');
-  //     }
-
-  //     var regexResult = tel.value.match(/(\+?\d[- .]*){7,13}/i);
-
-  //     if (!tel.value) {
-  //       isValidateSuccess = false;
-  //       tel.setCustomValidity('Телефон не может быть пустым.');
-  //     } else if (regexResult === null || regexResult.length <= 0) {
-  //       isValidateSuccess = false;
-  //       tel.setCustomValidity('Значение поля "Телефон" не удовлетворяет шаблону!');
-  //     }
-
-  //     if (isValidateSuccess !== true) {
-  //       name.reportValidity();
-  //       tel.reportValidity();
-
-  //       evt.preventDefault();
-  //     }
-  //   });
-  // }
 })();
