e25b9524f46546bfbe6d53985729d5291c7cb5d7
Alina Glebova <alina.lina2407@gmail.com>
2021-03-29 15:59:38 +0300

    Начала делать аккордеон в футере

--- source/js/main.js
+++ source/js/main.js
@@ -2,64 +2,60 @@
 
 (function () {
 
-  var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
-  var searchForm = document.querySelector('.form');
-  var submitBuuton = document.querySelector('.form__button');
-  var mainPage = document.querySelector('.page__main');
+  var citeButton = document.querySelector('.cite__button');
+  // var searchForm = document.querySelector('.form');
+  // var submitButton = document.querySelector('.form__button');
 
-  if (navMain && navToggle) {
-    navMain.classList.remove('main-nav__opened');
-    navMain.classList.add('main-nav__closed');
-    navMain.classList.remove('main-nav__nojs');
+  if (citeButton) {
+    citeButton.classList.remove('cite__button--opened');
+    citeButton.classList.add('cite__button--closed');
+    citeButton.classList.remove('cite__button--nojs');
 
-    navToggle.addEventListener('click', function () {
-      if (navMain.classList.contains('main-nav__closed')) {
-        navMain.classList.remove('main-nav__closed');
-        navMain.classList.add('main-nav__opened');
-        mainPage.classList.add('page__hidden');
+    citeButton.addEventListener('click', function () {
+      if (citeButton.classList.contains('cite__button--closed')) {
+        citeButton.classList.remove('cite__button--closed');
+        citeButton.classList.add('cite__button--opened');
       } else {
-        navMain.classList.add('main-nav__closed');
-        navMain.classList.remove('main-nav__opened');
-        mainPage.classList.remove('page__hidden');
+        citeButton.classList.add('cite__button--closed');
+        citeButton.classList.remove('cite__button--opened');
       }
     });
   }
 
 
-  if (searchForm) {
-    submitBuuton.addEventListener('click', function (evt) {
+  // if (searchForm) {
+  //   submitButton.addEventListener('click', function (evt) {
 
-      var isValidateSuccess = true;
+  //     var isValidateSuccess = true;
 
-      // получаем поля формы
-      var name = document.getElementById('name');
-      var tel = document.getElementById('tel');
+  //     // получаем поля формы
+  //     var name = document.getElementById('name');
+  //     var tel = document.getElementById('tel');
 
-      name.setCustomValidity('');
-      tel.setCustomValidity('');
+  //     name.setCustomValidity('');
+  //     tel.setCustomValidity('');
 
-      if (!name.value) {
-        isValidateSuccess = false;
-        name.setCustomValidity('Имя не может быть пустым.');
-      }
+  //     if (!name.value) {
+  //       isValidateSuccess = false;
+  //       name.setCustomValidity('Имя не может быть пустым.');
+  //     }
 
-      var regexResult = tel.value.match(/(\+?\d[- .]*){7,13}/i);
+  //     var regexResult = tel.value.match(/(\+?\d[- .]*){7,13}/i);
 
-      if (!tel.value) {
-        isValidateSuccess = false;
-        tel.setCustomValidity('Телефон не может быть пустым.');
-      } else if (regexResult === null || regexResult.length <= 0) {
-        isValidateSuccess = false;
-        tel.setCustomValidity('Значение поля "Телефон" не удовлетворяет шаблону!');
-      }
+  //     if (!tel.value) {
+  //       isValidateSuccess = false;
+  //       tel.setCustomValidity('Телефон не может быть пустым.');
+  //     } else if (regexResult === null || regexResult.length <= 0) {
+  //       isValidateSuccess = false;
+  //       tel.setCustomValidity('Значение поля "Телефон" не удовлетворяет шаблону!');
+  //     }
 
-      if (isValidateSuccess !== true) {
-        name.reportValidity();
-        tel.reportValidity();
+  //     if (isValidateSuccess !== true) {
+  //       name.reportValidity();
+  //       tel.reportValidity();
 
-        evt.preventDefault();
-      }
-    });
-  }
+  //       evt.preventDefault();
+  //     }
+  //   });
+  // }
 })();
